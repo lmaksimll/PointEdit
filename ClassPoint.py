4b01508b81decf2522fc7dd52f4db75670596256
Maksim Yurepin <yurepin.maks@mail.ru>
2024-01-02 03:20:13 +0300

    first commit

--- ClassPoint.py
+++
@@ -1,6 +0,0 @@
-class Point:
-    def __init__(self, x, y, index, number_file):
-        self.x = x
-        self.y = y
-        self.index = index
-        self.number_file = number_file
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import ClassField
-
-
 import tkinter as tk
+from tkinter import filedialog
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import csv
@@ -9,52 +7,54 @@ import csv
 class PointPlotterApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Point Plotter")
+        self.root.title("Point Edit")
 
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
-        self.canvas.get_tk_widget().pack()
+        self.canvas.get_tk_widget().grid(row=0, column=0, columnspan=3)
 
         self.points = []
 
         self.canvas.mpl_connect('button_press_event', self.on_click)
 
-        self.clear_button = tk.Button(self.root, text="Clear Points", command=self.clear_points)
-        self.clear_button.pack()
+        self.import_button = tk.Button(self.root, text="Import csv file", command=self.import_points, height=2)
+        self.import_button.grid(row=1, column=0, padx=15, pady=15)
 
-        self.show_button = tk.Button(self.root, text="Show Points", command=self.show_points)
-        self.show_button.pack()
+        self.export_button = tk.Button(self.root, text="Export csv file", command=self.export_points, height=2)
+        self.export_button.grid(row=1, column=1, padx=15, pady=15)
 
-        self.quit_button = tk.Button(self.root, text="Quit", command=self.quit_app)
-        self.quit_button.pack()
-
-        self.load_points_from_csv("PointCsv.csv")
+        self.delete_button = tk.Button(self.root, text="Apply changes", command=self.delete_points, height=2)
+        self.delete_button.grid(row=1, column=2, padx=15, pady=15)
 
     def on_click(self, event):
         for point in self.points:
             if abs(event.xdata - point[0]) < 0.1 and abs(event.ydata - point[1]) < 0.1:
+                self.ax.plot(point[0], point[1], 'gx')
                 self.points.remove(point)
-                self.plot_points()
+                self.canvas.draw()
                 return
 
-    def clear_points(self):
-        self.ax.clear()
-        self.points = []
-        self.canvas.draw()
 
-    def show_points(self):
-        if not self.points:
-            print("No points to display")
-        else:
-            for point in self.points:
-                print(f"({point[0]}, {point[1]})")
+    def import_points(self):
+        file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+        if file_path:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                reader = csv.DictReader(file, delimiter=';', quotechar='"')
+                for row in reader:
+                    x, y = float(row['x']), float(row['y'])
+                    self.points.append((x, y))
+            self.plot_points()
 
-    def load_points_from_csv(self, filename):
-        with open(filename, 'r', encoding='utf-8') as file:
-            reader = csv.DictReader(file, delimiter=';', quotechar='"')
-            for row in reader:
-                x, y = row['x'], row['y']
-                self.points.append((x, y))
+    def export_points(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
+        if file_path:
+            with open(file_path,'w',encoding='utf-8',newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=['x','y'], delimiter=';', quoting=csv.QUOTE_MINIMAL)
+                writer.writeheader()
+                for point in self.points:
+                    writer.writerow({'x': point[0], 'y': point[1]})
+
+    def delete_points(self):
         self.plot_points()
 
     def plot_points(self):
@@ -65,34 +65,6 @@ class PointPlotterApp:
 
         self.canvas.draw()
 
-    def quit_app(self):
-        self.root.quit()
-
-
 root = tk.Tk()
 app = PointPlotterApp(root)
 root.mainloop()
-
-
-#---------------------------------
-
-cf = ClassField.Field()
-
-btn = 'add_file'  # Нажатие кнопки
-name_file = 'PointCsv.csv'
-
-if btn == 'add_file':
-    cf.add_file(name_file)
-
-if btn == 'edit_file':
-    cf.edit_file(cf.list_file[0])
-
-if btn == 'delete_point':
-    del_list_points = []
-    cf.delete_point(del_list_points)
-
-
-
-
-
-
